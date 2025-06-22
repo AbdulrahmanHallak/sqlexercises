01946212cc2a94681c77a4bbd05f8dbbf99d987d
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-06-22 20:54:41 +0300

    initial implementation for exercise page
    since we do not know anything about the query that will be
    executed and we do not what data it will return, the best bet
    is to hash the result as it is and compare it with the stored
    solution. the solution is considered correct if it returned the same
    rows and the same columns in the same order. alias names does not affect
    the result. as for the hash, the requirements are to be FAST and
    deterministic and xxHash satisfies these requirements.

---
+++ src/SqlExercises.Razor/Pages/Categories/Exercises/Exercise.cshtml.cs
@@ -0,0 +1,103 @@
+using System.Data;
+using System.Text;
+using Dapper;
+using K4os.Hash.xxHash;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SqlExercises.Razor.Pages.Categories.Exercises;
+
+[IgnoreAntiforgeryToken]
+public class ExerciseModel(DapperContext context) : PageModel
+{
+    // For GET
+    [BindProperty(SupportsGet = true)]
+    public int Id { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string Category { get; set; }
+
+    public ExerciseDto Exercise { get; set; } = default!;
+    public IEnumerable<dynamic> ExpectedResult { get; set; }
+
+    // For POST
+
+    [BindProperty]
+    public string PostedSolution { get; set; }
+
+    public async Task<IActionResult> OnGet()
+    {
+        using var connection = context.CreateConnection();
+        var sql = """
+                SELECT id, question, answer, hint, solution
+                FROM exercise
+                WHERE id = @id
+            """;
+        var result = await connection.QuerySingleAsync<ExerciseDto>(sql, new { id = Id });
+        var expectedResults = await connection.QueryAsync(result.Solution);
+        if (result is null)
+            return NotFound();
+
+        Exercise = result;
+        ExpectedResult = expectedResults;
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (PostedSolution is null)
+            return new JsonResult(new { result = "No solution provided.", isEqual = false });
+
+        using var connection = context.CreateConnection();
+        var sql = @"SELECT solution FROM exercise WHERE id = @id";
+        var solution = await connection.QuerySingleAsync<string>(sql, new { id = Id });
+        var solutionResult = await connection.QueryAsync(solution);
+        var solutionString = StringifyDynamicList(solutionResult);
+        byte[] solutionData = Encoding.UTF8.GetBytes(solutionString);
+        var solutionHash = XXH64.DigestOf(solutionData);
+
+        IEnumerable<dynamic> resultRows;
+        try
+        {
+            resultRows = await connection.QueryAsync(PostedSolution);
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new { result = ex.Message, isEqual = false });
+        }
+        var resultString = StringifyDynamicList(resultRows);
+        byte[] resultData = Encoding.UTF8.GetBytes(resultString);
+        var resultHash = XXH64.DigestOf(resultData);
+
+        var isEqual = solutionHash.Equals(resultHash);
+
+        return new JsonResult(new { result = resultRows, isEqual });
+    }
+
+    private static string StringifyDynamicList(IEnumerable<dynamic> dynamics)
+    {
+        var typeSafe = dynamics
+            .Select(row => new Dictionary<string, object>((IDictionary<string, object>)row))
+            .ToList();
+
+        StringBuilder solString = new();
+        foreach (var row in typeSafe)
+        {
+            foreach (var column in row)
+            {
+                solString.Append($"{column.Key}{column.Value}");
+            }
+        }
+        return solString.ToString();
+    }
+
+    public class ExerciseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Question { get; set; } = default!;
+        public string Answer { get; set; } = default!;
+        public string Hint { get; set; } = default!;
+        public string Solution { get; set; } = default!;
+    }
+}
