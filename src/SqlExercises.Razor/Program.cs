1b3fc223dadfe3b0d1514656a1ada5d07570a800
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-06-24 21:33:35 +0300

    add serilog and seq

--- src/SqlExercises.Razor/Program.cs
+++ src/SqlExercises.Razor/Program.cs
@@ -1,55 +1,73 @@
 using Microsoft.AspNetCore.DataProtection;
+using Serilog;
 using SqlExercises.Razor;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Configuration.AddEnvironmentVariables();
-var connString =
-    builder.Configuration.GetConnectionString("Postgres")
-    ?? throw new InvalidOperationException("Connection string cannot be null");
-
-builder.Services.Configure<RouteOptions>(opts =>
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+try
 {
-    opts.LowercaseQueryStrings = true;
-    opts.LowercaseUrls = true;
-});
-builder
-    .Services.AddDataProtection()
-    .PersistKeysToFileSystem(
-        new DirectoryInfo(
-            Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                ".aspnet",
-                "DataProtection-Keys"
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog(
+        (context, config) => config.ReadFrom.Configuration(context.Configuration)
+    );
+
+    // Add services to the container.
+    builder.Services.AddRazorPages();
+    builder.Configuration.AddEnvironmentVariables();
+    var connString =
+        builder.Configuration.GetConnectionString("Postgres")
+        ?? throw new InvalidOperationException("Connection string cannot be null");
+
+    builder.Services.Configure<RouteOptions>(opts =>
+    {
+        opts.LowercaseQueryStrings = true;
+        opts.LowercaseUrls = true;
+    });
+    builder
+        .Services.AddDataProtection()
+        .PersistKeysToFileSystem(
+            new DirectoryInfo(
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    ".aspnet",
+                    "DataProtection-Keys"
+                )
             )
         )
-    )
-    .SetApplicationName("MyApp");
+        .SetApplicationName("MyApp");
 
-builder.Services.AddSingleton<ConnectionString>(_ => new ConnectionString(connString));
-builder.Services.AddSingleton<DapperContext>();
+    builder.Services.AddSingleton<ConnectionString>(_ => new ConnectionString(connString));
+    builder.Services.AddSingleton<DapperContext>();
 
-Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+    Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+    app.MapRazorPages().WithStaticAssets();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages().WithStaticAssets();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Server terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
