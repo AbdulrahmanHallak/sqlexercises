775bf4c5ea690cbce9884bd3c03d38b77e7083cc
Abdulrahman <abdulrahmanhallak04@gmail.com>
2025-09-06 19:20:37 +0300

    limit user access to only the current schema
    The user is not allowed to access anything when executing solutions
    other than the exercise's schema tables, hence this commit.
    It is worth noting that if the user prefixed tables names in the
    FROM clause with a schema name postgres will search for the table
    in the prefixed schema, which mean relying on search path is not
    secure at all but this will suffice until a better solution is
    implemented.

--- src/SqlExercises.Razor/ConnectionString.cs
+++ src/SqlExercises.Razor/ConnectionString.cs
@@ -1,8 +1,24 @@
 namespace SqlExercises.Razor;
 
+// * This will be used for everything else.
 public record ConnectionString(string Value)
 {
     public static implicit operator string(ConnectionString c) => c.Value;
 
     public static implicit operator ConnectionString(string s) => new(s);
+}
+
+// * This will be used only to run user submitted solutions.
+public record SolutionConnectionString
+{
+    public string Value { get; set; }
+
+    public SolutionConnectionString(string value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator string(SolutionConnectionString c) => c.Value;
+
+    public static implicit operator SolutionConnectionString(string s) => new(s);
 }
--- src/SqlExercises.Razor/DapperContext.cs
+++ src/SqlExercises.Razor/DapperContext.cs
@@ -6,11 +6,15 @@ namespace SqlExercises.Razor;
 public class DapperContext
 {
     private readonly ConnectionString _connectionString;
+    private readonly SolutionConnectionString _solutionString;
 
-    public DapperContext(ConnectionString connectionString)
+    public DapperContext(ConnectionString connectionString, SolutionConnectionString solutionString)
     {
         _connectionString = connectionString;
+        _solutionString = solutionString;
     }
 
     public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+
+    public IDbConnection CreateSolutionConnection() => new NpgsqlConnection(_solutionString);
 }
